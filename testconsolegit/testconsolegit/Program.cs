b5df12759ed0da9aa392dca1a6ef3eccf275946a
mazaycheck <suprunoleg88@gmail.com>
2020-02-04 16:21:54 +0200

    test2

--- testconsolegit/testconsolegit/Program.cs
+++ testconsolegit/testconsolegit/Program.cs
@@ -7,6 +7,7 @@ namespace testconsolegit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!1");
         }
     }
 }
