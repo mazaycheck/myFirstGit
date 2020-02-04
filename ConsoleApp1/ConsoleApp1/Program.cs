51aa8c1352d125ad6ebb52d701c00f71ec2259bc
mazaycheck <suprunoleg88@gmail.com>
2020-02-04 13:18:05 +0200

    2 commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!2");
+            Console.WriteLine("Hello World!1");
             Console.WriteLine("Hello World!2");
             Console.WriteLine("Hello World!3!");
             Console.WriteLine("Hello World!4!");
